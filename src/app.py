979da754f7967df8a3ec2f0a9ffabfe1a8507064
Joseph Fang <yfangfh15@gmail.com>
2017-08-27 17:54:33 -0700

    update app and add base.html

--- src/app.py
+++ src/app.py
@@ -1,33 +1,27 @@
-from flask import Flask
-from flask import render_template, request
+from flask import Flask, render_template, request
 import json
-import time
 from pymongo import MongoClient
 
 app = Flask(__name__)
 
+client = MongoClient()
+collection = client.streams.tweets
+
 @app.route('/')
 def hello():
-    return json.dumps('Hello!')
+    return render_template('base.html')
 
 @app.route('/summary',methods=['GET','POST'])
 def summary():
-    client = MongoClient()
-    collection = client.streams.tweets
-    ids = set()
-    total_counts = 0
-    positive_counts = 0
-    negative_counts = 0
-    for t in collection.find():
-        if t['id'] not in ids:
-            ids.add(t['id'])
-            total_counts += 1
-            if t['prediction'] == 1:
-                positive_counts += 1
-            else:
-                negative_counts += 1
-    client.close()
+    total_counts = collection.count()
+    negative_counts = collection.find({'prediction': 0}).count()
+    positive_counts = collection.find({'prediction': 1}).count()
     return render_template('result.html',total=total_counts, positive=positive_counts, negative=negative_counts) 
 
+@app.route('/table', methods=['GET', 'POST'])
+def table():
+    neg_tweets = collection.find({'prediction': 0})
+    return render_template('table.html', tweets=list(neg_tweets))
+        
 if __name__ == '__main__':
     app.run(debug=True)
